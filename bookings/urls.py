4de4a726c73470e408dbbd3c787db572a456c148
unknown <akshitsahore2827@gmail.com>
2025-08-21 09:39:44 +0530

    Created Homepage and Registeration form

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    path('register/', views.register_user, name='register_user'),
+]
--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib import messages
 # Create your views here.
+def homepage(request):
+    return render(request, 'Homepage.html')
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, '🎉 Welcome to Travel Haven! Your account has been created successfully.')
+            return redirect('combined_dashboard')
+    else:
+        form = UserCreationForm()
+    return render(request, 'Registeration_Form.html', {"form": form})
